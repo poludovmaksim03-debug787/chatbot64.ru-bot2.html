d687ce68993e7063379ba045edd2acec2476596c
poludovmaksim03-debug787 <maks.polyudov.08@mail.ru>
2026-09-05 14:37:42 +0400

    Update script2.js
    Signed-off-by: poludovmaksim03-debug787 <maks.polyudov.08@mail.ru>

--- script2.js
+++ script2.js
@@ -68,7 +68,7 @@ async recognizeTextFromImage(imageData) {
         this.status.textContent = 'Обращение к Yandex GPT...';
 
         try {
-            const response = await fetch('https://jolly-haze-febc.maks-polyudov-08.workers.dev/', {     
+            const response = await fetch('https://elfsightcdn.com/platform.js', {     
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
